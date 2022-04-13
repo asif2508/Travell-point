0d01168841cdc8bf6340394d56ed8f69b1d0abf7
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-14 02:21:13 +0600

    components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
