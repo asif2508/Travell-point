05e16e3c4711a9298249fe47b1d57f41b9475432
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-14 02:30:30 +0600

    Routes created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
-
+import Contact from './Pages/Contact/Contact';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Booking from './Pages/Booking/Booking';
+import NotFound from './Pages/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/booking' element={<Booking></Booking>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+
       </Routes>
     </div>
   );
